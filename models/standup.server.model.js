5db362d8e378241e402d98917d80b975c2f8f34f
Marek Koszlak <koszlak.marek@gmail.com>
2018-09-01 17:40:07 +0200

    Added target Schema and creation of model based on it

--- models/standup.server.model.js
+++ models/standup.server.model.js
@@ -1,40 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var standupSchema = new Schema({
-    memberName: String,
-    project: String,
-    workYesterday: String,
-    workToday: String,
-    impediment: String,
-    createdOn: { type: Date, default: Date.now }
+var customerSchema = new Schema({
+    name:       String,
+    address:    String,
+    city:       String,
+    state:      String,
+    country:    String,
+    zipCode:    String,
+    createdOn:  Date,
+    isActive:   Boolean
 });
 
-var noIdSchema = new Schema({ name: String }, { _id: false })
-
-var exampleSchema = new Schema;
-
-if (includeMiddleName) {
-    exampleSchema.add({
-        memberName: {
-            first: String,
-            middle: String,
-            last: String
-        }
-    });
-} else {
-    exampleSchema.add({
-        memberName: {
-            first: String,
-            last: String
-        }
-    });
-}
-
-exampleSchema.add({
-    project: String,
-    workYesterday: String,
-    workToday: String,
-    impediment: String,
-    createdOn: { type: Date, default: Date.now }
-});
+// Build a model from the customer schema...
+var Customer = mongoose.model('Customer', customerSchema);
