fc6cd671b1e060fd5220ff0ce4fcc0863c921e51
Marek Koszlak <koszlak.marek@gmail.com>
2018-09-01 17:26:43 +0200

    Added models catalog and connection to mongo

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://mkoszlak:testpwd123@cluster0-tsn1p.mongodb.net/test?retryWrites=true');
+
 var app = express();
 
 var swig = require('swig');
