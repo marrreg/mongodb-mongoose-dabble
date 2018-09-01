8226b1161825425488c84bc4c7128d00e9a96080
Marek Koszlak <koszlak.marek@gmail.com>
2018-09-01 17:55:04 +0200

    Added controllers and routes handling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var standupCtrl = require('../controllers/standup.server.controller.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/newnote', function(req, res) {
+  return standupCtrl.getNode(req, res);
+})
 
 module.exports = router;
